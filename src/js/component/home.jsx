860e5a7ed8b12056dd4925dcddf44028959936d4
Ashley Molina <131078848+Ashley8-prog@users.noreply.github.com>
2023-08-02 18:33:16 +0000

    Cambiostodolist02agosto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from "react";
 //create your first component
 const Home = () => {
 	const [tarea, setTarea] = useState("")
-	const [lista, setLista] = useState(["Make the bed", "Wash my hands"])
+	const [lista, setLista] = useState([])
 
 	const handleInput = (e) => {
 		let texto = e.target.value
@@ -29,14 +29,14 @@ const Home = () => {
 
 	return (
 		<>
-		<div className="container text-center mt-5 caja display-5"> <p>todos</p> </div>
-			<div className="container caja"> 
-				<div className=" justify-content-start contenedor mt-2 shadow p-3 mb-5 bg-body">
-					<input className="container p-2 " placeholder="What needs to be done?"
+			<div className="container text-center mt-5 caja display-5"> <p>todos</p> </div>
+			<div className="container caja">
+				<div className=" justify-content-start contenedor mt-2 shadow mb-5 bg-body">
+					<input className="container p-3 mb-2" placeholder="What needs to be done? Add here"
 						onKeyUp={
 							(e) => { handleInput(e) }
 						} />
-					<div>
+					<div className="paper">
 						<ul className="list-group list-group-flush">
 							{
 								lista && lista.length > 0 ?
@@ -49,15 +49,19 @@ const Home = () => {
 												</button>
 											</li>
 										})
-									}</>
-									
-									: "la lista está vacía"
-							}
+									}
+									</>
 
+									: " "
+							}
+							<p className="items bg-bg-dark" >  {lista.length} items left </p>
 						</ul>
 					</div>
+
+
+
 				</div>
-				
+
 			</div>
 		</>
 
